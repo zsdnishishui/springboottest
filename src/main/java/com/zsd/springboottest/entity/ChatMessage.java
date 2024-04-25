829a40fb3ad4085ac242361bd519c24dac065623
周圣道 <zsdnishishui@163.com>
2024-04-25 17:10:07 +0800

    改成LocalDateTime

--- src/main/java/com/zsd/springboottest/entity/ChatMessage.java
+++ src/main/java/com/zsd/springboottest/entity/ChatMessage.java
@@ -4,13 +4,12 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Data
 public class ChatMessage {
     private String sender;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private Date timestamp;
+    private LocalDateTime timestamp;
     private String text; // Getters and setters
 }
