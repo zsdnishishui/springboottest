304f05c26e35d70c91f35b396260d7ce03dfc0c7
周圣道 <zsdnishishui@163.com>
2024-05-09 14:16:52 +0800

    利用@EventListener注解实现项目启动初始

--- src/main/java/com/zsd/springboottest/listener/MyAnnotationListener.java
+++ src/main/java/com/zsd/springboottest/listener/MyAnnotationListener.java
@@ -1,5 +1,6 @@
 package com.zsd.springboottest.listener;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,12 @@ public class MyAnnotationListener {
     @EventListener
     public void listener1(MyTestEvent event) {
         System.out.println("注解监听器1:" + event.getMsg());
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReadyEvent(ApplicationReadyEvent event) {
+        System.out.println("Spring Boot应用已启动并准备就绪，开始执行初始化操作...");
+        // 在这里执行需要在应用启动后进行的初始化代码
     }
 }
 
