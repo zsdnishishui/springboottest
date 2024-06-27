6b1743e3e079051fc4162e9dcaac6d2a2aa94e9a
周圣道 <zsdnishishui@163.com>
2024-06-27 16:47:24 +0800

    开启跨域，开启后@RequestMapping默认只会允许get,post

--- src/main/java/com/zsd/springboottest/controller/HelloController.java
+++ src/main/java/com/zsd/springboottest/controller/HelloController.java
@@ -3,13 +3,12 @@ package com.zsd.springboottest.controller;
 import com.zsd.springboottest.listener.MyTestEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zsd.service.ServiceHello;
 
 @RestController
 @RequestMapping("/base")
+@CrossOrigin
 public class HelloController {
 
     @Autowired
@@ -24,8 +23,7 @@ public class HelloController {
     }
 
 
-
-    @RequestMapping(value = "/test/testPublishEvent1")
+    @RequestMapping(value = "/test/testPublishEvent1", method = {RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST})
     public void testPublishEvent() {
         // 默认是同步调用
         // 如果改成异步参考：https://blog.csdn.net/yubaojin/article/details/115569767
